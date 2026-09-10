d744eb4dc94a7f897784bdf6a4a7c22d9b1cfdae
garretgonzales <garret915@gmail.com>
2026-09-10 13:27:38 -0500

    feat(app) added validated text echo endpoint

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class AnalysisRequest(BaseModel):
+    text: str
+
+
+
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.post("/analyze")
+def analyze(request: AnalysisRequest):
+    # Perform analysis on the text
+    return {"text" : request.text}
 
